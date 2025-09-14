7b0565b2f9c686d7d612d2ea1646122c62a81a7a
Ziba Nouri <zibanouri13621115@gmail.com>
2025-09-14 11:43:33 +0330

    add prettierignore

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,6 @@
 const Index = () => {
   return (
-    <h1 className="text-3xl font-bold text-red-500 underline">Hello world!</h1>
+    <h1 className="text-2xl font-bold text-blue-500">Hello world Ziba!</h1>
   );
 };
 
