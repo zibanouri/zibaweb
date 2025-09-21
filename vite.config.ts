2bca06ef81b87516ac36f7cd2d224b5ac4991e7c
Ziba Nouri <zibanouri13621115@gmail.com>
2025-09-21 14:49:47 +0330

    update in config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
-  base:'/zibanouri/',
+  base: '/zibanouri/',
   plugins: [
     react(),
-     tailwindcss()
-    ],
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
